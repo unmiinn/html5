9799a2be63efc0b0f35ecb1d907101c3c402472b
unmiinn <eunmincase@naver.com>
2025-06-26 13:44:12 +0900

    재재업로드

--- wonderfulworld/src/App.jsx
+++ wonderfulworld/src/App.jsx
@@ -4,9 +4,16 @@ function App() {
 
   return (
     <div className="center_div">
-      <h1>Wonderfu1 W0r1d</h1>
-      <h2>~ 아름ㄷr운 Sㅔ상 ~ </h2>
-      <h3>심신안정ㅇ1 필요할 때 보zr</h3>
+      <h2>Wonderfu1 W0r1d</h2>
+      <h2>안녕하세요 이은민입니다</h2>
+      <h3>~ 아름ㄷr운 Sㅔ상 ~ </h3>
+      <h4>심신안정ㅇ1 필요할 때 보zr</h4>
+      <h6 className="you">고도희 박태범 조현서 현명진 바보</h6>
+      <h6 className="you">이은민 킹왕짱천재</h6>
+      <h6 className="mun">
+        이것쫌ㅂr신기하지문어마녀ㄱr이러케만드럿ㅈ1<br></br>
+        인간0l되고ㅅ1펏던날고앵이로만드럿지
+      </h6>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/B9VRvOKKwfs?si=cyJRuqZRqd_v86r7&amp;start=7442" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
       <iframe width="560" height="315" src="https://www.youtube.com/embed/NHo_x7aGGmU?si=0VHjQX5ptkTo9RTW&amp;start=288" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
