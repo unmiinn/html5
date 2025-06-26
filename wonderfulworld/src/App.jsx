5d3bac28965eb9e0c6b53e06d7eeb2cc67c98de0
unmiinn <eunmincase@naver.com>
2025-06-26 13:18:53 +0900

    재업로드

--- wonderfulworld/src/App.jsx
+++ wonderfulworld/src/App.jsx
@@ -3,14 +3,18 @@ function App() {
   const apple = 1
 
   return (
-    <div>
+    <div className="center_div">
       <h1>Wonderfu1 W0r1d</h1>
       <h2>~ 아름ㄷr운 Sㅔ상 ~ </h2>
-      <h3>심신안정ㅇ1 필요할 때 보zr</h3><br></br>
+      <h3>심신안정ㅇ1 필요할 때 보zr</h3>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/B9VRvOKKwfs?si=cyJRuqZRqd_v86r7&amp;start=7442" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
       <iframe width="560" height="315" src="https://www.youtube.com/embed/NHo_x7aGGmU?si=0VHjQX5ptkTo9RTW&amp;start=288" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
       <iframe width="560" height="315" src="https://www.youtube.com/embed/MxcJtLbIhvs?si=i7UDITpdABRizjgD&amp;start=1026" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
       <iframe width="560" height="315" src="https://www.youtube.com/embed/OfO6zxvhtBg?si=ZiiA_WaLD2ZO0uy7&amp;start=912" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
       <iframe width="560" height="315" src="https://www.youtube.com/embed/taJr7qZGHPI?si=A4u5TGIpuMK0o1SN&amp;start=4153" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/KLuTLF3x9sA?si=WIbzsuqbTbW_zPzn&amp;start=23103" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
